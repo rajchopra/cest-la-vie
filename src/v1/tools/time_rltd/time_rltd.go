cdc437ae50d600a0a5f4b98469e84610ac9c7233
Raj Vimal Chopra <raj.chopra10@gmail.com>
2017-01-18 00:26:02 +0530

    Some more formats to print time/date

--- src/v1/tools/time_rltd/time_rltd.go
+++ src/v1/tools/time_rltd/time_rltd.go
@@ -10,5 +10,24 @@ func main() {
 	fmt.Println(t.Format("2006-01-02-15:04:05"))
 	fmt.Println(t.Format("2006-01-02 15:04:05"))
 	fmt.Println(t.Format("2006-01-02<raj>15:04:05"))
+	fmt.Println(time.Now().Format(time.RFC850))
+	fmt.Println(time.Now().Format(time.RFC3339))
 
+	// more samples
+	p := fmt.Println
+	p(t.Format(time.RFC3339))
+	t1, e := time.Parse(
+		time.RFC3339,
+		"2012-11-01T22:08:41+00:00")
+	p(t1)
+	p(t.Format("3:04PM"))
+	p(t.Format("Mon Jan _2 15:04:05 2006"))
+	p(t.Format("2006-01-02T15:04:05.999999-07:00"))
+	form := "3 04 PM"
+	t2, e := time.Parse(form, "8 41 PM")
+	p(t2)
+	fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
+		t.Year(), t.Month(), t.Day(),
+		t.Hour(), t.Minute(), t.Second())
+	p("error =", e)
 }
