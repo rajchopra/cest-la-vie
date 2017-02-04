8950fa7c9295cb59904469bd0ed6edc4d3df402d
Raj Vimal Chopra <raj.chopra10@gmail.com>
2017-02-04 23:48:44 +0530

    Testing out goRoutines with variable sleep time in test function

---
+++ src/v1/test/goRoutines/goRoutineX/goRoutineX.go
@@ -0,0 +1,49 @@
+// A _goroutine_ is a lightweight thread of execution.
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func f(from string, duration time.Duration) {
+	for i := 0; i < 20; i++ {
+		fmt.Println(from, ":", i)
+		time.Sleep(duration * time.Millisecond)
+	}
+}
+
+func main() {
+
+	// Suppose we have a function call `f(s)`. Here's how
+	// we'd call that in the usual way, running it
+	// synchronously.
+	f("direct", 100)
+
+	// To invoke this function in a goroutine, use
+	// `go f(s)`. This new goroutine will execute
+	// concurrently with the calling one.
+	go f("goroutine", 500)
+
+	// start 20 goroutines of function f with different sleep duration
+	for i := 0; i < 20; i++ {
+		go f(strings.Join([]string{"Raj"}, strconv.Itoa(i)), time.Duration(50*(20-i)))
+	}
+
+	// You can also start a goroutine for an anonymous
+	// function call.
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	// Our two function calls are running asynchronously in
+	// separate goroutines now, so execution falls through
+	// to here. This `Scanln` code requires we press a key
+	// before the program exits.
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println("done")
+}
